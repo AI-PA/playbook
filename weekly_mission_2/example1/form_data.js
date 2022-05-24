1d29225e8258a6de8b8bebb6f388ccd6363a45f0
Alan Paz  (AIPA) <israel.pazar@gmail.com>
2022-05-23 23:28:06 -0500

    Ejercicio 1 Moldeo de Informacion

---
+++ weekly_mission_2/example1/form_data.js
@@ -0,0 +1,38 @@
+// Twitter
+const  user_twitter= {
+    user_baner:" ",
+    user_img:"owl.jpg",
+    name: "AIPA 01",
+    user_name:"@02AIPA",
+    bio:"Text~...",
+    location:"México",
+    joined:"02/2022",
+    following: 88,
+    follower: 1,
+    genelarinfo : function(){
+        return `
+        User Name: ${this.user_name}
+        Name: ${this.name}
+        Location: ${this.location}
+        Followers: ${this.follower}`
+    }
+}
+
+const trading_topic_twitter ={
+    location: "Mexico",
+    first:["Football","Blanca Félix"],
+    second:["Entreteinment","#ThorLoveAndThunder"],
+    third:["Only on Twitter","#FelizLunes"],
+    fourth:["Treding","#HotSale"],
+    fifth:["Treding","#DiaDelEstudiante"]
+ }
+
+
+const hashtag_twitter ={
+    user_img:"Marvel.jpg",
+    name:"Marvel Entreteinment",
+    verrified:"Yes",
+    user_name:"@Marvel",
+    twitet_data: "52m",
+    message:"...~ #ThorLoveAndThunder ~... "
+}
